7a4ef226708ee741fca8ab82f120bf6229ce479f
Zelxel-zx <z.bravo016@gmail.com>
2025-06-26 12:03:12 -0500

    feat: se añadio profesores a la data del js

--- js/datacursos.js
+++ js/datacursos.js
@@ -3,102 +3,119 @@ export const dataCursos = [
         nombre: "Introducción a la IA",
         descripcion: "Aprenda los conceptos básicos de la IA y sus aplicaciones.",
         imagen: "assets/imagenes_cursos/introducion a la IA.jpg",
+        profesor: "Dr. Juan Pérez",
         pro: false
     },
     {
         nombre: "Técnicas avanzadas de IA",
         descripcion: "Profundice en los algoritmos y modelos avanzados de la IA.",
         imagen: "assets/imagenes_cursos/tecnicas avanzadas de IA.jpg",
+        profesor: "Dra. Ana Gómez",
         pro: true
     },
     {
         nombre: "Ética y Sociedad",
         descripcion: "Explora las implicaciones éticas y sociales de la inteligencia artificial.",
         imagen: "assets/imagenes_cursos/Etica y sociedad de la IA.png",
+        profesor: "Dr. Luis Martínez",
         pro: false
     },
     {
         nombre: "Transformación Digital",
         descripcion: "Descubre cómo la tecnología está cambiando los modelos de negocio y la sociedad.",
         imagen: "assets/imagenes_cursos/IA en los negocios.jpg",
+        profesor: "Dra. Laura Fernández",
         pro: false
     },
     {
         nombre: "Fundamentos para Principiantes",
         descripcion: "Un curso introductorio para quienes inician en el mundo de la tecnología y la IA.",
         imagen: "assets/imagenes_cursos/IA para principiantes.jpeg",
+        profesor: "Dr. Carlos López",
         pro: false
     },
     {
         nombre: "Gestión de Proyectos Tecnológicos",
         descripcion: "Aprende a planificar y gestionar proyectos tecnológicos de manera efectiva.",
         imagen: "assets/imagenes_cursos/gestion de proyectos de IA.jpg",
+        profesor: "Dra. María Rodríguez",
         pro: false
     },
     {
         nombre: "Data Science",
         descripcion: "Descubre cómo analizar datos y obtener información valiosa para la toma de decisiones.",
         imagen: "assets/imagenes_cursos/IA y data science.jpg",
+        profesor: "Dr. Andrés Torres",
         pro: true
     },
     {
         nombre: "Tecnología en Salud",
         descripcion: "Explora el impacto de la tecnología en la industria de la salud y la medicina.",
         imagen: "assets/imagenes_cursos/IA en Healthcare.jpg",
+        profesor: "Dra. Elena Ruiz",
         pro: false
     },
     {
         nombre: "Tecnología en Finanzas",
         descripcion: "Comprende el papel de la tecnología en el sector financiero y la innovación en servicios.",
         imagen: "assets/imagenes_cursos/IA y finanzas.jpg",
+        profesor: "Dr. Javier Sánchez",
         pro: false
     },
     {
         nombre: "Marketing Digital",
         descripcion: "Aprovecha las herramientas digitales para potenciar estrategias de marketing.",
         imagen: "assets/imagenes_cursos/IA y marketing.jpg",
+        profesor: "Dra. Paula Jiménez",
         pro: false
     },
     {
         nombre: "Tecnología en Educación",
         descripcion: "Aplica herramientas tecnológicas para mejorar procesos de enseñanza y aprendizaje.",
         imagen: "assets/imagenes_cursos/IA y eduacion.jpg",
+        profesor: "Dr. Roberto Castro",
         pro: false
     },
     {
         nombre: "Tecnología y Sostenibilidad",
         descripcion: "Utiliza la tecnología para impulsar soluciones sostenibles y cuidar el medio ambiente.",
         imagen: "assets/imagenes_cursos/IA y sostenibilidad.jpg",
+        profesor: "Dra. Sofía Herrera",
         pro: false
     },
     {
         nombre: "Fundamentos de IA",
         descripcion: "Aprenda los conceptos básicos de la IA y sus aplicaciones.",
         imagen: "assets/imagenes_cursos/Fundamentos de IA.jpg",
+        profesor: "Dr. German Gonzalez",
         pro: false
     },
     {
         nombre: "Fundamentos de Machine Learning",
         descripcion: "Introducción a los algoritmos y técnicas de machine learning.",
         imagen: "assets/imagenes_cursos/Fundamentos de machine learning.jpg",
+        profesor: "Dra. Ana Torres",
         pro: true
     },
     {
         nombre: "Fundamentos de Deep Learning",
         descripcion: "Explore los conceptos centrales de deep learning.",
         imagen: "assets/imagenes_cursos/Fundamentos de deep learning.jpg",
+        profesor: "Dr. Juan Carlos",
         pro: true
     },
     {
         nombre: "Procesamiento de Lenguaje Natural",
         descripcion: "Comprende cómo procesar y analizar el lenguaje humano con tecnología.",
         imagen: "assets/imagenes_cursos/procesamiento de natural language.jpg",
+        profesor: "Dra. Laura Martínez",
         pro: true
     },
     {
         nombre: "Visión por Computadora",
         descripcion: "Introducción a la visión artificial y al reconocimiento de imágenes.",
         imagen: "assets/imagenes_cursos/computer-vision.png",
+        profesor: "Dr. Joel Torres",
         pro: true
     }
 ];
